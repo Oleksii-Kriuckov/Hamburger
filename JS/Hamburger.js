3bb9f316cbab0ae4d828d3c12f5ef5bc0b00d73c
Oleksii-Kriuckov <75043251+Oleksii-Kriuckov@users.noreply.github.com>
2021-08-01 22:51:16 +0300

    Add files via upload

--- JS/Hamburger.js
+++ JS/Hamburger.js
@@ -17,7 +17,6 @@ function Parameters(name, price, calories) {
     this.paramName = name;
     this.price = price;
     this.calories = calories;
-    this.added = false;
 }
 
 Hamburger.SIZE_SMALL = new Parameters("small", 50, 20);
@@ -26,56 +25,124 @@ Hamburger.STUFFING_CHEESE = new Parameters("cheese", 10, 20);
 Hamburger.STUFFING_SALAD = new Parameters("salad", 20, 5);
 Hamburger.STUFFING_POTATO = new Parameters("potato", 15, 10);
 Hamburger.TOPPING_MAYO = new Parameters("mayo", 20, 5);
+Hamburger.TOPPING_MAYO.added = false;
 Hamburger.TOPPING_SPICE = new Parameters("spice", 15, 0);
+Hamburger.TOPPING_SPICE.added = false;
 
 let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO)
-console.log(hamburger)
 console.log("price: " + hamburger.hamburgerPrice())
 console.log("calories: " + hamburger.hamburgerCalories())
 
-pPrice.innerHTML = hamburger.hamburgerPrice()
-pCalories.innerHTML = hamburger.hamburgerCalories()
+let hamburgerPrice = hamburger.hamburgerPrice()
+let hamburgerCalories = hamburger.hamburgerCalories()
 
-let price = hamburger.hamburgerPrice()
-let calories = hamburger.hamburgerCalories()
+pPrice.innerHTML = hamburgerPrice
+pCalories.innerHTML = hamburgerCalories
 
 Hamburger.prototype.addTopping = function (topping) {
-    for (let prop in hamburger) {
-        if (prop == topping) {
-            this.topping = true;
-        } else {
-            this.topping = false;
-        }
-    }
-    if (this.topping == false) {
-        this.topping = topping;
-        // let price = this.hamburgerPrice();
-        this.totalPrice = function () {
-            return price + this.topping.price;
-        }
-        price = this.totalPrice()
 
-        //let calories = this.hamburgerCalories()
-        this.totalCalories = function () {
-            return calories + this.topping.calories;
+    if (topping.added === false) {
+        if (topping.paramName == "mayo") {
+            this.toppingMayo = topping;
+            topping.added = true;
+        } else {
+            this.toppingSpice = topping;
+            topping.added = true;
         }
-        calories = this.totalCalories()
+
+        this.totalPrice = function () {
+            return hamburgerPrice + topping.price;
+        }
+        this.totalCalories = function () {
+            return hamburgerCalories + topping.calories;
+        }
+        hamburgerPrice = this.totalPrice()
+        hamburgerCalories = this.totalCalories()
+        pPrice.innerHTML = hamburgerPrice
+        pCalories.innerHTML = hamburgerCalories
     }
 }
+
+Hamburger.prototype.removeTopping = function (topping) {
+    if (topping.added === true) {
+        if (topping.paramName == "mayo") {
+            delete this.toppingMayo;
+            topping.added = false;
+        } else {
+            delete this.toppingSpice;
+            topping.added = false;
+        }
+
+        this.totalPrice = function () {
+            return hamburgerPrice - topping.price;
+        }
+        this.totalCalories = function () {
+            return hamburgerCalories - topping.calories;
+        }
+
+        hamburgerPrice = this.totalPrice()
+        hamburgerCalories = this.totalCalories()
+        pPrice.innerHTML = hamburgerPrice
+        pCalories.innerHTML = hamburgerCalories
+    }
+}
+
+
+Hamburger.prototype.getToppings = function () {
+    var addedTopping = new Array(), i = 0;
+    for (let prop in this) {
+        if (this[prop].added === true) {
+            addedTopping[i] = this[prop].paramName;
+            i++;
+        }
+    }
+    return addedTopping;
+}
+
+Hamburger.prototype.getSize = function () {
+    return this.size.paramName
+}
+
+Hamburger.prototype.getStuffing = function () {
+    return this.stuffing.paramName
+}
+
+Hamburger.prototype.calculatePrice = function () {
+    return hamburgerPrice
+}
+
+Hamburger.prototype.calculateCalories = function () {
+    return hamburgerCalories
+}
+
 hamburger.addTopping(Hamburger.TOPPING_MAYO)
-console.log(hamburger)
-console.log("price: " + price)
-console.log("calories: " + calories)
+console.log("price: " + hamburgerPrice)
+console.log("calories: " + hamburgerCalories)
 
 hamburger.addTopping(Hamburger.TOPPING_SPICE)
-console.log(hamburger)
-console.log("price: " + price)
-console.log("calories: " + calories)
+
+console.log("price: " + hamburgerPrice)
+console.log("calories: " + hamburgerCalories)
 
 hamburger.addTopping(Hamburger.TOPPING_MAYO)
+console.log("price: " + hamburgerPrice)
+console.log("calories: " + hamburgerCalories)
+
+console.log("All topping: " + hamburger.getToppings().join(", "))
+
+// hamburger.removeTopping(Hamburger.TOPPING_MAYO)
+// console.log("price: " + price)
+// console.log("calories: " + calories)
+// console.log("All topping: " + hamburger.getToppings().join(", "))
+
+// hamburger.removeTopping(Hamburger.TOPPING_SPICE)
 console.log(hamburger)
-console.log("price: " + price)
-console.log("calories: " + calories)
+console.log("All topping: " + hamburger.getToppings().join(", "))
+console.log("Hamburger size: " + hamburger.getSize())
+console.log("Hamburger with stuffing: " + hamburger.getStuffing())
+console.log("Hamburger price: " + hamburger.calculatePrice())
+console.log("Hamburger calories: " + hamburger.calculateCalories())
+
 /**
  * Добавить добавку к гамбургеру. Можно добавить несколько
 * добавок, при условии, что они разные.
