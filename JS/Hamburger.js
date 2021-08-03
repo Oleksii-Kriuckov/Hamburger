ab7ffb1152498850c44318fb11012c9aa1e3a4ae
Oleksii-Kriuckov <75043251+Oleksii-Kriuckov@users.noreply.github.com>
2021-08-03 22:54:27 +0300

    Add files via upload

--- JS/Hamburger.js
+++ JS/Hamburger.js
@@ -2,47 +2,52 @@ let pPrice = document.querySelector(".price")
 let pCalories = document.querySelector(".calories")
 
 function Hamburger(size, stuffing) {
-    this.size = size;
-    this.stuffing = stuffing;
+    try {
+        if (!size || !stuffing) {
+            throw new HamburgerException('MissingData', "no size or stuffing given");
+        }
+        if (size.paramType != "Size" || stuffing.paramType != "Stuffing") {
+            throw new HamburgerException('UncorectedParameters', "invalid size or stuffing");
+        }
 
-    this.hamburgerPrice = function () {
-        return this.size.price + this.stuffing.price;
-    }
-    this.hamburgerCalories = function () {
-        return this.size.calories + this.stuffing.calories;
+        this.size = size;
+        this.stuffing = stuffing;
+    } catch (err) {
+        alert(err.name + ": " + err.message);
     }
 }
 
-function Parameters(name, price, calories) {
+
+function Parameters(type, name, price, calories) {
+    this.paramType = type;
     this.paramName = name;
     this.price = price;
     this.calories = calories;
 }
 
-Hamburger.SIZE_SMALL = new Parameters("small", 50, 20);
-Hamburger.SIZE_LARGE = new Parameters("large", 100, 40);
-Hamburger.STUFFING_CHEESE = new Parameters("cheese", 10, 20);
-Hamburger.STUFFING_SALAD = new Parameters("salad", 20, 5);
-Hamburger.STUFFING_POTATO = new Parameters("potato", 15, 10);
-Hamburger.TOPPING_MAYO = new Parameters("mayo", 20, 5);
+Hamburger.SIZE_SMALL = new Parameters("Size", "Small", 50, 20);
+Hamburger.SIZE_LARGE = new Parameters("Size", "Large", 100, 40);
+Hamburger.STUFFING_CHEESE = new Parameters("Stuffing", "Cheese", 10, 20);
+Hamburger.STUFFING_SALAD = new Parameters("Stuffing", "Salad", 20, 5);
+Hamburger.STUFFING_POTATO = new Parameters("Stuffing", "Potato", 15, 10);
+Hamburger.TOPPING_MAYO = new Parameters("Topping", "Mayo", 20, 5);
 Hamburger.TOPPING_MAYO.added = false;
-Hamburger.TOPPING_SPICE = new Parameters("spice", 15, 0);
+Hamburger.TOPPING_SPICE = new Parameters("Topping", "Spice", 15, 0);
 Hamburger.TOPPING_SPICE.added = false;
 
-let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO)
-console.log("price: " + hamburger.hamburgerPrice())
-console.log("calories: " + hamburger.hamburgerCalories())
 
-let hamburgerPrice = hamburger.hamburgerPrice()
-let hamburgerCalories = hamburger.hamburgerCalories()
-
-pPrice.innerHTML = hamburgerPrice
-pCalories.innerHTML = hamburgerCalories
 
 Hamburger.prototype.addTopping = function (topping) {
 
-    if (topping.added === false) {
-        if (topping.paramName == "mayo") {
+    try {
+        if (!topping) {
+            throw new HamburgerException('MissingData', "no topping given");
+        }
+        if (topping.added === true) {
+            throw new HamburgerException('DuplicateTopping', `Topping ${topping.paramName} has been already added`)
+        }
+        
+        if (topping.paramName == "Mayo") {
             this.toppingMayo = topping;
             topping.added = true;
         } else {
@@ -50,22 +55,22 @@ Hamburger.prototype.addTopping = function (topping) {
             topping.added = true;
         }
 
-        this.totalPrice = function () {
-            return hamburgerPrice + topping.price;
-        }
-        this.totalCalories = function () {
-            return hamburgerCalories + topping.calories;
-        }
-        hamburgerPrice = this.totalPrice()
-        hamburgerCalories = this.totalCalories()
-        pPrice.innerHTML = hamburgerPrice
-        pCalories.innerHTML = hamburgerCalories
+    } catch (err) {
+        alert(err.name + ": " + err.message);
     }
 }
 
+
 Hamburger.prototype.removeTopping = function (topping) {
-    if (topping.added === true) {
-        if (topping.paramName == "mayo") {
+    try {
+        if (!topping) {
+            throw new HamburgerException('MissingData', "no topping given");
+        }
+        if (topping.added === false) {
+            throw new HamburgerException('MissingTopping', `Topping ${topping.paramName} was not added`)
+        }
+
+        if (topping.paramName == "Mayo") {
             delete this.toppingMayo;
             topping.added = false;
         } else {
@@ -73,20 +78,10 @@ Hamburger.prototype.removeTopping = function (topping) {
             topping.added = false;
         }
 
-        this.totalPrice = function () {
-            return hamburgerPrice - topping.price;
-        }
-        this.totalCalories = function () {
-            return hamburgerCalories - topping.calories;
-        }
-
-        hamburgerPrice = this.totalPrice()
-        hamburgerCalories = this.totalCalories()
-        pPrice.innerHTML = hamburgerPrice
-        pCalories.innerHTML = hamburgerCalories
+    } catch (err) {
+        alert(err.name + ": " + err.message);
     }
 }
-
 
 Hamburger.prototype.getToppings = function () {
     var addedTopping = new Array(), i = 0;
@@ -108,34 +103,62 @@ Hamburger.prototype.getStuffing = function () {
 }
 
 Hamburger.prototype.calculatePrice = function () {
-    return hamburgerPrice
+    let price = 0;
+    for (let prop in this) {
+        if (this[prop].price) {
+            price = this[prop].price + price;
+        }
+    }
+    pPrice.innerHTML = price;
+    return price;
 }
+
 
 Hamburger.prototype.calculateCalories = function () {
-    return hamburgerCalories
+    let calories = 0;
+    for (let prop in this) {
+        if (this[prop].calories) {
+            calories = this[prop].calories + calories;
+        }
+    }
+    pCalories.innerHTML = calories;
+    return calories;
 }
 
+function HamburgerException(name, message) {
+    this.name = name;
+    this.message = message;
+}
+
+
+let h1 = new Hamburger()
+let h2 = new Hamburger(Hamburger.TOPPING_MAYO, Hamburger.TOPPING_MAYO)
+let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO)
+
+let hamburgerPrice = hamburger.calculatePrice()
+let hamburgerCalories = hamburger.calculateCalories()
+
+hamburger.addTopping()
 hamburger.addTopping(Hamburger.TOPPING_MAYO)
-console.log("price: " + hamburgerPrice)
-console.log("calories: " + hamburgerCalories)
+console.log("price: " + hamburger.calculatePrice())
+console.log("calories: " + hamburger.calculateCalories())
+
 
 hamburger.addTopping(Hamburger.TOPPING_SPICE)
-
-console.log("price: " + hamburgerPrice)
-console.log("calories: " + hamburgerCalories)
+console.log("price: " + hamburger.calculatePrice())
+console.log("calories: " + hamburger.calculateCalories())
 
 hamburger.addTopping(Hamburger.TOPPING_MAYO)
-console.log("price: " + hamburgerPrice)
-console.log("calories: " + hamburgerCalories)
 
 console.log("All topping: " + hamburger.getToppings().join(", "))
 
-// hamburger.removeTopping(Hamburger.TOPPING_MAYO)
-// console.log("price: " + price)
-// console.log("calories: " + calories)
-// console.log("All topping: " + hamburger.getToppings().join(", "))
+hamburger.removeTopping()
+hamburger.removeTopping(Hamburger.TOPPING_MAYO)
+hamburger.removeTopping(Hamburger.TOPPING_MAYO)
+console.log("price: " + hamburger.calculatePrice())
+console.log("calories: " + hamburger.calculateCalories())
+console.log("All topping: " + hamburger.getToppings().join(", "))
 
-// hamburger.removeTopping(Hamburger.TOPPING_SPICE)
 console.log(hamburger)
 console.log("All topping: " + hamburger.getToppings().join(", "))
 console.log("Hamburger size: " + hamburger.getSize())
@@ -143,65 +166,3 @@ console.log("Hamburger with stuffing: " + hamburger.getStuffing())
 console.log("Hamburger price: " + hamburger.calculatePrice())
 console.log("Hamburger calories: " + hamburger.calculateCalories())
 
-/**
- * Добавить добавку к гамбургеру. Можно добавить несколько
-* добавок, при условии, что они разные.
-*
-* @param topping     Тип добавки
-* @throws {HamburgerException}  При неправильном использовании
-
-Hamburger.prototype.addTopping = function (topping) ...
-*/
-
-/**
- * Убрать добавку, при условии, что она ранее была
- * добавлена.
- *
- * @param topping   Тип добавки
- * @throws {HamburgerException}  При неправильном использовании
-
-Hamburger.prototype.removeTopping = function (topping) ...
-*/
-
-/**
- * Получить список добавок.
- *
- * @return {Array} Массив добавленных добавок, содержит константы
- *                 Hamburger.TOPPING_*
-
-Hamburger.prototype.getToppings = function () ...
-*/
-
-/**
- * Узнать размер гамбургера
-
-Hamburger.prototype.getSize = function () ...
-*/
-
-/**
- * Узнать начинку гамбургера
-
-Hamburger.prototype.getStuffing = function () ...
-*/
-
-/**
- * Узнать цену гамбургера
- * @return {Number} Цена в тугриках
-
-Hamburger.prototype.calculatePrice = function () ...
-*/
-
-/**
- * Узнать калорийность
- * @return {Number} Калорийность в калориях
-
-Hamburger.prototype.calculateCalories = function () ...
-*/
-
-/**
- * Представляет информацию об ошибке в ходе работы с гамбургером.
- * Подробности хранятся в свойстве message.
- * @constructor
-
-function HamburgerException (...) { ... }
-```*/
