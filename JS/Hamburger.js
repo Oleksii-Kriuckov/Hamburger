e0ba3d3f8c4f6d161beba3ff789d718033cf3533
Oleksii-Kriuckov <75043251+Oleksii-Kriuckov@users.noreply.github.com>
2021-08-08 11:01:49 +0300

    Add files via upload

--- JS/Hamburger.js
+++ JS/Hamburger.js
@@ -1,5 +1,10 @@
-let pPrice = document.querySelector(".price")
-let pCalories = document.querySelector(".calories")
+const pPrice = document.querySelector(".price")
+const pCalories = document.querySelector(".calories")
+const pToppings = document.querySelector(".toppings")
+const checkboxMayo = document.querySelector('#topping_mayo');
+const checkboxSpice = document.querySelector('#topping_spice');
+const pGetSize = document.querySelector(".size");
+const pGetStuffing = document.querySelector(".stuffing");
 
 function Hamburger(size, stuffing) {
     try {
@@ -9,14 +14,13 @@ function Hamburger(size, stuffing) {
         if (size.paramType != "Size" || stuffing.paramType != "Stuffing") {
             throw new HamburgerException('UncorectedParameters', "invalid size or stuffing");
         }
-        
+
         this.size = size;
         this.stuffing = stuffing;
     } catch (err) {
         alert(err.name + ": " + err.message);
     }
 }
-
 
 function Parameters(type, name, price, calories) {
     this.paramType = type;
@@ -38,17 +42,17 @@ Hamburger.TOPPING_SPICE.added = false;
 
 
 Hamburger.prototype.addTopping = function (topping) {
-    
+
     try {
         if (!topping) {
             throw new HamburgerException('MissingData', "no topping given");
-        } else if (topping.type != "Topping") {
+        } else if (topping.paramType != "Topping") {
             throw new HamburgerException('UncorectedParameters', "invalid topping");
         }
         if (topping.added === true) {
             throw new HamburgerException('DuplicateTopping', `Topping ${topping.paramName} has been already added`)
         }
-        
+
         if (topping.paramName == "Mayo") {
             this.toppingMayo = topping;
             topping.added = true;
@@ -56,12 +60,11 @@ Hamburger.prototype.addTopping = function (topping) {
             this.toppingSpice = topping;
             topping.added = true;
         }
-        
+
     } catch (err) {
         alert(err.name + ": " + err.message);
     }
 }
-
 
 Hamburger.prototype.removeTopping = function (topping) {
     try {
@@ -134,40 +137,92 @@ function HamburgerException(name, message) {
     this.message = message;
 }
 
+const buttonCalculate = document.querySelector("#calculate");
+buttonCalculate.addEventListener('click', (ev) => {
+    let inputSize, inputStuffing, hamburgerSize, hamburgerStuffng;
+    inputSize = document.querySelectorAll('[name="size"]');
+    inputSize.forEach((el) => {
+        if (el.checked == true) {
+            if (el.value == "small") {
+                hamburgerSize = Hamburger.SIZE_SMALL
+            } else if (el.value == "large") {
+                hamburgerSize = Hamburger.SIZE_LARGE
+            }
+        }
+    })
+    inputStuffing = document.querySelectorAll('[name="stuffing"]');
+    inputStuffing.forEach((el) => {
+        if (el.checked == true) {
+            switch (el.value) {
+                case "cheese": {
+                    hamburgerStuffng = Hamburger.STUFFING_CHEESE
+                } break;
+                case "salad": {
+                    hamburgerStuffng = Hamburger.STUFFING_SALAD
+                } break;
+                case "potato": {
+                    hamburgerStuffng = Hamburger.STUFFING_POTATO
+                }
+            }
+        }
+    })
 
-let h1 = new Hamburger()
-let h2 = new Hamburger(Hamburger.TOPPING_MAYO, Hamburger.TOPPING_MAYO)
-let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO)
+    let hamburger = new Hamburger(hamburgerSize, hamburgerStuffng)
+    Hamburger.TOPPING_MAYO.added = false;
+    Hamburger.TOPPING_SPICE.added = false
+    pToppings.innerHTML="";
+    pGetSize.innerHTML ="";
+    pGetStuffing.innerHTML ="";
+    
+    if (checkboxMayo.checked == true && !hamburger.toppingMayo) {
+        hamburger.addTopping(Hamburger.TOPPING_MAYO)
+    }
 
-let hamburgerPrice = hamburger.calculatePrice()
-let hamburgerCalories = hamburger.calculateCalories()
+    if (checkboxSpice.checked == true && !hamburger.toppingSpice) {
+        hamburger.addTopping(Hamburger.TOPPING_SPICE)
+    }
 
-hamburger.addTopping()
-hamburger.addTopping(Hamburger.TOPPING_MAYO)
-console.log("price: " + hamburger.calculatePrice())
-console.log("calories: " + hamburger.calculateCalories())
+    hamburger.calculatePrice()
+    hamburger.calculateCalories()
+
+    const buttonAllTopping = document.querySelector("#all_toppings")
+    buttonAllTopping.addEventListener('click', () => {
+        pToppings.innerHTML = hamburger.getToppings().join(", ");
+    })
+
+    const buttonGetSize = document.querySelector("#get_size");
+    buttonGetSize.addEventListener('click', () => {
+        pGetSize.innerHTML = hamburger.getSize();
+    })
+    
+    const buttonGetStuffing = document.querySelector("#get_stuffing");
+    buttonGetStuffing.addEventListener('click', () => {
+        pGetStuffing.innerHTML = hamburger.getStuffing();
+    })
+    
+})
 
 
-hamburger.addTopping(Hamburger.TOPPING_SPICE)
-console.log("price: " + hamburger.calculatePrice())
-console.log("calories: " + hamburger.calculateCalories())
+// Для проверки
+// let h1 = new Hamburger()
+// let h2 = new Hamburger(Hamburger.TOPPING_MAYO, Hamburger.TOPPING_MAYO)
+//  let hamburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE)
 
-hamburger.addTopping(Hamburger.TOPPING_MAYO)
+// hamburger.addTopping()
+// hamburger.addTopping(Hamburger.STUFFING_SALAD)
+// hamburger.addTopping(Hamburger.TOPPING_MAYO)
+// hamburger.addTopping(Hamburger.TOPPING_SPICE)
+// console.log("price: " + hamburger.calculatePrice())
+// console.log("calories: " + hamburger.calculateCalories())
+// console.log("All topping: " + hamburger.getToppings().join(", "))
 
-console.log("All topping: " + hamburger.getToppings().join(", "))
+// hamburger.removeTopping()
+// hamburger.removeTopping(Hamburger.SIZE_SMALL)
+// hamburger.removeTopping(Hamburger.TOPPING_MAYO)
+// hamburger.removeTopping(Hamburger.TOPPING_MAYO)
+// hamburger.removeTopping(Hamburger.TOPPING_SPICE)
+// console.log("price: " + hamburger.calculatePrice())
+// console.log("calories: " + hamburger.calculateCalories())
+// console.log("All topping: " + hamburger.getToppings().join(", "))
 
-hamburger.removeTopping()
-hamburger.removeTopping(Hamburger.TOPPING_MAYO)
-hamburger.removeTopping(Hamburger.TOPPING_MAYO)
-hamburger.removeTopping(Hamburger.TOPPING_MAYO)
-console.log("price: " + hamburger.calculatePrice())
-console.log("calories: " + hamburger.calculateCalories())
-console.log("All topping: " + hamburger.getToppings().join(", "))
-
-console.log(hamburger)
-console.log("All topping: " + hamburger.getToppings().join(", "))
-console.log("Hamburger size: " + hamburger.getSize())
-console.log("Hamburger with stuffing: " + hamburger.getStuffing())
-console.log("Hamburger price: " + hamburger.calculatePrice())
-console.log("Hamburger calories: " + hamburger.calculateCalories())
-
+// console.log(hamburger)
